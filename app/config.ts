2391ea7f5fac386623b4baf0eb9c82d1dc4d506c
Sunil Rana <sunil28rana@gmail.com>
2020-09-26 03:05:05 +0530

    Minor tweak in config file

--- app/config.ts
+++ app/config.ts
@@ -13,8 +13,8 @@ switch (process.env.NODE_ENV) {
 
 dotenv.config({ path: path })
 
-// export const DATABASE_URI: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-export const DATABASE_URI: string = `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+export const DATABASE_URI: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+// export const DATABASE_URI: string = `mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 export const PORT: string | number = process.env.PORT || 3000
 
 console.log(process.env.MONGO_DB)
